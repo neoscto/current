e8bbeec1670dbf833e051614f56a4baedc629d82
Trushar Narodia <trushar.narodia@bytestechnolab.com>
2024-01-31 17:52:04 +0530

    Header, Footer and learn more page UI changes

--- app/description/page.tsx
+++ app/description/page.tsx
@@ -10,66 +10,75 @@ const Description = () => {
   const router = useRouter();
   const { t } = useTranslation();
 
+
+
   return (
     <MainContainer>
-      <div className="relative rounded-[30px] bg-[#01092299] max-w-[1200px] md:max-w-[88%] lg:max-w-[1200px] w-full mx-auto bg-white overflow-hidden">
-        <div className="flex items-center gap-x-[12px] absolute top-[1.1em] left-[.8em] md:top-[1.6em] md:left-[1.6em]">
-          {/* <span onClick={() => router.back()}>
-            <ArrowBackIcon
-              className="mb-[0.35em] cursor-pointer "
-              sx={{ fontSize: "30px" }}
-            />
-          </span> */}
-        </div>
-        <div className="w-full mx-auto bg-white  flex items-center gap-[3px] flex-col-reverse md:flex-row lg:flex-row justify-center pt-[72px] pb-[75px] pl-[51px]">
-          <div className="text-center md:text-left lg:text-left max-w-[calc(60%_-_3px)] w-full">
-            <h1 className="text-lg md:2xl lg:text-3xl font-bold">
+      <div className="relative rounded-[30px] bg-[#01092299] md:w-full max-w-[calc(100%_-_40px)] xl:max-w-[1200px] w-full mx-auto bg-white overflow-hidden">
+
+        <div className="w-full mx-auto bg-white  flex lg:flex-row flex-col items-center lg:gap-[23px] justify-center pt-[72px] lg:pb-[75px] lg:pl-[51px] lg:pr-4 md:pt-[34px] pb-[40px] pl-4 pr-6 gap-[27px]">
+          <div className="text-center md:text-left lg:text-left lg:max-w-[calc(62%_-_13px)] w-full max-w-full">
+            <h1 className="text-lg md:2xl lg:text-3xl font-bold text-center lg:leading-9">
               {t("Description.title")}
             </h1>
 
-            <div className="w-full py-[13px] pr-[9px] pl-[16px] rounded-[8px] border-[1px] border-[#E0E0E0] mt-1.5">
-              <p className="text-sm md:text-base lg:text-base text-black">
-                {t("Description.description")}
+            <div className="w-full py-[13px] pr-[9px] pl-[16px] rounded-[8px] border-[1px] border-[#E0E0E0] mt-1.5 md:mt-2.5">
+              <p className="text-base md:text-base text-[#4F4F4F] lg:text-xl leading-[25.2px] text-left" dangerouslySetInnerHTML={{ __html: t("Description.description") }}>
               </p>
             </div>
-            <div className="my-4 md:my-8 lg:my-8 flex flex-wrap gap-[18px] ">
-              {descriptionList.map((item, index) => {
-                if (index % 4 === 1 || index % 4 === 2) {
-                  return (<div className="flex flex-col gap-1 items-center max-w-[calc(50%_-_9px)] w-full pt-[19px] pb-[12px] bg-[#E7F5FA] rounded-3xl ">
-                    <p
-                      className="text-sm md:text-base lg:text-base text-black font-bold "
-                      dangerouslySetInnerHTML={{
-                        __html: t(`Description.${Object.keys(item)[0]}`),
-                      }}
-                    />
-                    <p className="text-center h-[90px] px-1 flex justify-center items-center text-black text-lg leading-5">{`${t(`Description.${Object.keys(item)[1]}`)}`}</p>
-                  </div>
-                  )
-                }
-                else {
 
-                  return (<div className="flex flex-col gap-1 items-center max-w-[calc(50%_-_9px)] w-full pt-[19px] pb-[12px] bg-[#1D3E6A] rounded-3xl ">
-                    <p
-                      className="text-sm md:text-base lg:text-base text-white font-bold "
-                      dangerouslySetInnerHTML={{
-                        __html: t(`Description.${Object.keys(item)[0]}`),
-                      }}
-                    />
-                    <p className="text-center h-[90px] px-1 flex justify-center items-center text-white text-lg leading-5">{`${t(`Description.${Object.keys(item)[1]}`)}`}</p>
-                  </div>
-                  )
-                }
+            <div className="flex flex-col items-center justify-center lg:hidden mt-3">
+              <img src="virtual-solar-small.png" alt="Description image" width={267} />
+              <div className="text-center mt-[23px]">
+              <NeosButton
+                className={'px-[24px] py-[14px] text-sm leading-4 font-semibold'}
+                category="colored"
+                title={t("Calculate-saving-btn")}
+                onClick={() => router.push("/getoffer")}
+              />
+            </div>
+            </div>
 
-              })}
+            <div className="lg:mt-[29px] mt-[27px]">
+              <h1 className="text-lg md:2xl lg:text-3xl font-bold text-center leading-9">{t("Description.Benefits")}</h1>
+              <div className="flex flex-wrap gap-[18px] sm:gap-4 mt-5 lg:mt-2.5">
+                {descriptionList.map((item, index) => {
+                  if (index % 4 === 1 || index % 4 === 2) {
+                    return (<div className={`flex flex-col gap-2.5 lg:gap-1 items-center max-w-full lg:max-w-[calc(50%_-_9px)] w-full lg:px-0 p-4 lg:pt-[19px] lg:pb-[12px] lg:bg-[#E7F5FA] rounded-3xl sm:max-w-full ${index % 2 === 0 ? 'bg-[#1D3E6A]' : 'bg-[#E7F5FA]'}`}>
+                      <p
+                        className={`text-xl lg:text-2xl lg:leading-7 lg:!text-black font-bold ${index % 2 === 0 ? 'text-white' : 'text-black'}`}
+                        dangerouslySetInnerHTML={{
+                          __html: t(`Description.${Object.keys(item)[0]}`),
+                        }}
+                      />
+                      <p className={`text-center lg:min-h-[90px] md:h-auto px-1 flex justify-center items-center lg:text-black lg:text-lg text-base leading-5 ${index % 2 === 0 ? 'text-white' : 'text-black'}`}>{`${t(`Description.${Object.keys(item)[1]}`)}`}</p>
+                    </div>
+                    )
+                  }
+                  else {
+                    return (<div className={`flex flex-col gap-2.5 lg:gap-1 items-center max-w-full lg:max-w-[calc(50%_-_9px)] w-full lg:px-0 p-4 lg:pt-[19px] lg:pb-[12px] lg:bg-[#1D3E6A] rounded-3xl  ${index % 2 === 0 ? 'bg-[#1D3E6A]' : 'bg-[#E7F5FA]'}`}>
+                      <p
+                        className={`text-xl lg:text-2xl lg:leading-7 lg:!text-white font-bold ${index % 2 === 0 ? 'text-white' : 'text-black'} `}
+                        dangerouslySetInnerHTML={{
+                          __html: t(`Description.${Object.keys(item)[0]}`),
+                        }}
+                      />
+                      <p className={`text-center lg:min-h-[90px] px-1 flex justify-center items-center lg:!text-white lg:text-lg text-base leading-5 ${index % 2 === 0 ? 'text-white' : 'text-black'}`}>{`${t(`Description.${Object.keys(item)[1]}`)}`}</p>
+                    </div>
+                    )
+                  }
+
+                })}
+              </div>
             </div>
 
           </div>
 
-          <div className="ml-auto relative">
-            <img src="description.png" alt="Description image" />
-            <div className="text-center absolute bottom-[-17px] left-1/2 -translate-x-2/4">
+          <div className="lg:ml-auto relative ml-0 lg:max-w-[calc(38%_-_13px)] w-full flex lg:flex-col items-center ">
+            <img src="description.png" alt="Description image" width={425} className="lg:flex hidden" />
+            <div className="lg:mt-[3px]">
               <NeosButton
-                className={'min-w-[233px] w-full px-[24px] py-[17px] text-base leading-5 font-semibold'}
+                className={'px-[24px] lg:py-[17px] py-[14px] text-sm leading-4 font-semibold'}
                 category="colored"
                 title={t("Calculate-saving-btn")}
                 onClick={() => router.push("/getoffer")}
