0cc8c03d5859291dc0ea7a75a98cc641ab453538
Tahseen-Neos <tahseen@neosenergy.co>
2024-02-23 01:26:08 +0600

    feat: refine cookie banner

--- components/AcceptCookie.tsx
+++ components/AcceptCookie.tsx
@@ -26,6 +26,18 @@ const AcceptCookie = () => {
   return (
     <>
       {show && (
+        <div className="fixed bottom-0 w-full bg-white bg-opacity-75 border-t border-gray-300 p-2 flex items-center justify-center text-sm">
+          <p className="text-blue-900">{t("Home.cookie.description")}</p>
+          <button
+            onClick={handleAccept}
+            className="ml-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none"
+          >
+            {t("Home.cookie.acceptBtn")}
+          </button>
+        </div>
+      )}
+
+      {/* {show && (
         <div className="fixed bottom-0 w-full bg-[#000000] text-white p-4 text-center z-10 flex justify-center items-center gap-4">
           <p className=" text-base">{t("Home.cookie.description")}</p>
           <button
@@ -35,7 +47,7 @@ const AcceptCookie = () => {
             {t("Home.cookie.acceptBtn")}
           </button>
         </div>
-      )}
+      )} */}
     </>
   );
 };
