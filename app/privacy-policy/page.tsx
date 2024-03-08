26714dd64f05cc1d5033c9fabec363c7b249a369
Tahseen-Neos <tahseen@neosenergy.co>
2024-03-08 20:21:33 +0600

    fit: added term of use content

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import MainContainer from "@/components/sharedComponents/MainContainer";
-import { Box, Typography } from "@mui/material";
-import { contentData } from "./constant";
+'use client';
+import React from 'react';
+import MainContainer from '@/components/sharedComponents/MainContainer';
+import { Box, Typography } from '@mui/material';
+import { contentData } from './constant';
 
 const privacyPolicy = () => {
   return (
     <MainContainer>
-      <Box sx={{ width: "100%" }}>
+      <Box sx={{ width: '100%' }}>
         <div className="rounded-[30px] bg-[#01092299] max-w-[93%] md:max-w-[88%] lg:max-w-[83%] w-full mx-auto bg-white">
           <div className="w-[94%] mx-auto py-6 md:py-9 lg:py-9">
             <Typography variant="h4" className="text-center" gutterBottom>
@@ -15,9 +16,9 @@ const privacyPolicy = () => {
             <div
               className="scroll-bar"
               style={{
-                height: "49vh",
+                height: '49vh',
                 // overflowY: "scroll",
-                paddingRight: "0.4rem",
+                paddingRight: '0.4rem'
               }}
             >
               <Typography className="whitespace-break-spaces leading-[26px]">
