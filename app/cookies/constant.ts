381ae2b5dd7d0c3c32eb8f7cc1b8b574bcfde37e
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-04-02 16:25:38 +0500

    complete cookie policy

---
+++ app/cookies/constant.ts
@@ -0,0 +1,11 @@
+export const cookiesData = [
+  {
+    subtitle0: 'INTRODUCTION'
+  },
+  {
+    subtitle1: 'COOKIES'
+  },
+  {
+    subtitle2: 'CLASSIFICATION OF COOKIES'
+  }
+];
--- app/cookies/page.tsx
+++ app/cookies/page.tsx
@@ -2,25 +2,32 @@
 import React from 'react';
 import MainContainer from '@/components/sharedComponents/MainContainer';
 import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
+import { cookiesData } from './constant';
+import parse from 'html-react-parser';
 
 const cookies = () => {
+  const { t } = useTranslation();
   return (
     <MainContainer>
       <Box sx={{ width: '100%' }}>
         <div className="rounded-[30px] bg-[#01092299] max-w-[93%] md:max-w-[88%] lg:max-w-[83%] w-full mx-auto bg-white">
           <div className="w-[94%] mx-auto py-6 md:py-9 lg:py-9">
             <Typography variant="h4" className="text-center" gutterBottom>
-              COOKIES
+              {t('cookies.title')}
             </Typography>
-            <div
-              className="scroll-bar"
-              style={{
-                height: '49vh',
-                // overflowY: "scroll",
-                paddingRight: '0.4rem'
-              }}
-            >
-              Coming soon...
+            <div className=" flex flex-col gap-4">
+              {cookiesData.map((data, index) => (
+                <div key={index}>
+                  <h1 className=" font-bold text-lg">
+                    {' '}
+                    {t(`cookies.subtitle${index}`)}
+                  </h1>
+                  <p> {parse(t(`cookies.content${index}`))}</p>
+                </div>
+              ))}
+              {/* <Typography>Coming soon...</Typography> */}
             </div>
           </div>
         </div>
