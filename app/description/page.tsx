d0b95afa5d204ef6b77ad0d2002e64b9f0286a83
“neoscto” <karim@neosenergy.co>
2024-04-02 17:16:03 +0100

    button issue on desktop

--- app/description/page.tsx
+++ app/description/page.tsx
@@ -48,7 +48,7 @@ const Description = () => {
               </Button>
             </div>
 
-            <div className="flex flex-col justify-center items-center mt-4 mb-6">
+            <div className="flex flex-col justify-center items-center mt-4">
               <h1 className=" text-2xl font-bold mt-6 mb-4">
                 {t('Description.benefits')}
               </h1>
