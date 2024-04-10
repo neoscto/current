ed3a2e68b8f29924cc6dd39fc010384b2dd9cc3c
“neoscto” <karim@neosenergy.co>
2024-04-10 17:04:05 +0100

    footer correction

--- components/sharedComponents/Footer.tsx
+++ components/sharedComponents/Footer.tsx
@@ -26,7 +26,6 @@ const Footer = () => {
               <MailOutlineIcon />
               <span>contact@neosenergy.co</span>
             </Link>
-
             <Link
               href="tel:+34 900 732 890"
               className="flex gap-1.5 items-center invert brightness-0"
@@ -39,8 +38,6 @@ const Footer = () => {
               <Link
                 href="https://www.instagram.com/neos.energy/"
                 className="invert brightness-0"
-                target="_blank"
-                rel="noopener noreferrer"
               >
                 <InstagramIcon />
               </Link>
@@ -48,17 +45,12 @@ const Footer = () => {
               <Link
                 href="https://www.linkedin.com/company/neos-energy/"
                 className="invert brightness-0"
-                target="_blank"
-                rel="noopener noreferrer"
               >
                 <LinkedInIcon />
               </Link>
-
               <Link
                 href="https://twitter.com/Neos_Energy"
                 className="invert brightness-0"
-                target="_blank"
-                rel="noopener noreferrer"
               >
                 <XIcon />
               </Link>
@@ -71,7 +63,36 @@ const Footer = () => {
           </div>
 
           {/* middle Container */}
-          {/* ... */}
+          <div className="hidden md:flex flex-col gap-4 justify-center items-center max-w-[calc(33%_-_8px)] w-full">
+            <Image
+              src={neoslogo}
+              alt="Neos logo"
+              width={100}
+              className="h-[20px] md:h-auto lg:h-auto -ms-1 md:ms-0 lg:ms-0 cursor-pointer object-contain"
+              priority
+            />
+            <div className="flex flex-row gap-2">
+              <Link
+                href="https://www.instagram.com/neos.energy/"
+                className="invert brightness-0"
+              >
+                <InstagramIcon />
+              </Link>
+
+              <Link
+                href="https://www.linkedin.com/company/neos-energy/"
+                className="invert brightness-0"
+              >
+                <LinkedInIcon />
+              </Link>
+              <Link
+                href="https://twitter.com/Neos_Energy"
+                className="invert brightness-0"
+              >
+                <XIcon />
+              </Link>
+            </div>
+          </div>
 
           {/* Right Container */}
           <div className="md:max-w-[calc(33%_-_8px)] w-full flex flex-col gap-2 md:items-end items-center">
