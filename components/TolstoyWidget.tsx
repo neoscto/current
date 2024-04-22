5d004e10e8525e9772747667e15a478f0b8a0e61
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-04-22 11:14:53 +0500

    make widget fixed

--- components/TolstoyWidget.tsx
+++ components/TolstoyWidget.tsx
@@ -2,10 +2,10 @@
 
 const TolstoyWidget = ({ src }: { src: string }) => {
   return (
-    <div className="absolute right-5 bottom-2">
+    <div className="fixed right-2 bottom-2">
       <iframe
         src={src}
-        className="hidden md:block md:w-[125px] md:h-[125px] xl:w-[150px] xl:h-[150px] rounded-full"
+        className="w-[75px] h-[75px] md:w-[125px] md:h-[125px] xl:w-[150px] xl:h-[150px] rounded-full"
         scrolling="no"
         frameBorder="0"
         allow="autoplay *; clipboard-write *;camera *; microphone *; encrypted-media *; fullscreen *; display-capture *;"
