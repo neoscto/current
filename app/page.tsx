6f5227c22d467dc45773b1180e24c4bf02305ed8
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-05-02 21:59:17 +0500

    take down the site

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,15 @@
-'use client';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchPosts } from '@/features/common/commonSlice';
-import { AppDispatch, RootState } from '@/store/store';
-import LandingPage from './landingpage/page';
-import Launch from './launch/page';
-import { useTranslation } from 'react-i18next';
-interface postData {
-  title: string;
-}
-
 export default function Home() {
-  const dispath = useDispatch<AppDispatch>();
-  // const { post } = useSelector((state: RootState) => state.commonSlice)
-
-  // console.log(post)
-  // useEffect(() => {
-  //   dispath(fetchPosts())
-  // }, [])
-
-  // useEffect(() => {
-  //   (window as any).tolstoyWidgetId = "8n325wid8kupn";
-  //   var s = document.createElement("script");
-  //   s.type = "text/javascript";
-  //   s.async = true;
-  //   s.src = "https://widget.gotolstoy.com/widget/widget.js";
-  //   document.head.appendChild(s);
-  // }, []);
-
-  const { t } = useTranslation();
-  const { language }: any = useSelector(
-    (state: RootState) => state.commonSlice
-  );
-
-  useEffect(() => {
-    document.title = t('title');
-  }, [language]);
-
   return (
-    <div className="w-full relative">
-      <LandingPage />
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-700 mb-2">
+          Site Under Maintenance
+        </h1>
+        <p className="text-gray-500">
+          We&apos;re currently performing some maintenance. We&apos;ll be back
+          soon!
+        </p>
+      </div>
     </div>
   );
 }
