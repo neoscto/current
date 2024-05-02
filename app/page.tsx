70d2d51c29f7406ece35e72ab71ed6ae161e739d
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-05-02 22:00:48 +0500

    increase title font size

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
       <div className="text-center">
-        <h1 className="text-3xl font-bold text-gray-700 mb-2">
+        <h1 className="text-4xl font-bold text-gray-700 mb-2">
           Site Under Maintenance
         </h1>
         <p className="text-gray-500">
