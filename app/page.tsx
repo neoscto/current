d25a40c8cee371397f382ec5617a11f8aa44ce67
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2024-05-02 22:20:24 +0500

    fix: mobile screen view

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
       <div className="text-center">
-        <h1 className="text-4xl font-bold text-gray-700 mb-2">
+        <h1 className="text-[26px] sm:text-4xl font-bold text-gray-700 mb-2 px-2">
           Site Under Maintenance
         </h1>
-        <p className="text-gray-500">
+        <p className="text-gray-500 px-4">
           We&apos;re currently performing some maintenance. We&apos;ll be back
           soon!
         </p>
